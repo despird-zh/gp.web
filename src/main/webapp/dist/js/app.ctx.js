5552dc34008013f9239e389be8437dd3b02e6f57
gary <gary.diao@yahoo.com>
2016-05-10 22:58:07 +0800

    add GenerateUID function to support pseudo UUID for browser side code

--- src/main/webapp/dist/js/app.ctx.js
+++ src/main/webapp/dist/js/app.ctx.js
@@ -778,8 +778,19 @@ GPContext = (function ($) {
 	};
 	
 	_context.initial();
+	
+	var _generateUid = function (separator) {
+		var delim = separator || "-";
 
+		function S4() {
+			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+		}
+
+		return (S4() + S4() + delim + S4() + delim + S4() + delim + S4() + delim + S4() + S4() + S4());
+	};
+	
 	return {
-		Relogon : _context.relogon
+		Relogon : _context.relogon,
+		GenerateUID : _generateUid
 	};
 })(jQuery);
