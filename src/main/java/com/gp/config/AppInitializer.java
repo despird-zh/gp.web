2010c2cdd564e530934181445d822d68b7b4d304
gary-dgc <garydiao@localhost>
2016-05-17 18:11:14 +0800

    add comment to AppInitializer

--- src/main/java/com/gp/config/AppInitializer.java
+++ src/main/java/com/gp/config/AppInitializer.java
@@ -45,23 +45,25 @@ public class AppInitializer implements WebApplicationInitializer {
         rootContext.register(ServiceConfigurator.class);
         // since we registered RootConfig instead of passing it to the constructor
         rootContext.refresh(); 
-        // Manage the lifecycle of the root appcontext
+        // Manage the life cycle of the root application context
         servletContext.addListener(new ContextLoaderListener(rootContext));
-        
+        // Add context listeners
         servletContext.addListener(new CoreStarter());        
         servletContext.addListener(new RequestContextListener());
-
+        // Add encoding filter
         FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter",  
         	      new CharacterEncodingFilter());
         	   fr.setInitParameter("encoding", "UTF-8");
         	   fr.setInitParameter("forceEncoding", "true");
         	   fr.addMappingForUrlPatterns(null, true, "/*");
         	   
+        // Add security shiro filter
         FilterRegistration.Dynamic fr1 = servletContext.addFilter("shiroFilter",  
          	      new DelegatingFilterProxy());
          	   fr1.setInitParameter("targetFilterLifecycle", "true");
          	   fr1.addMappingForUrlPatterns(null, true, "/*"); 
-        
+         	   
+        // Add image cache filter
         FilterRegistration.Dynamic fr2 = servletContext.addFilter("GroupressImageFilter",  
               	      new ImageFilter());
               	   fr2.addMappingForUrlPatterns(null, true, "/img_cache/*"); 
@@ -81,13 +83,13 @@ public class AppInitializer implements WebApplicationInitializer {
         // Add the servlet mapping manually and make it initialize automatically
         TransferServlet transferServlet = new TransferServlet();
         ServletRegistration.Dynamic servlet1 = servletContext.addServlet("TransferServlet", transferServlet);
-        
         servlet1.setInitParameter("upload_path", "d:\\");
         servlet1.addMapping("/transfer");
         servlet1.setAsyncSupported(true);
         servlet1.setMultipartConfig( getMultiPartConfig() );
         servlet1.setLoadOnStartup(2);
         
+        // Add avatar image crop servlet handle resize and rotate process
         AvatarServlet avatarServlet = new AvatarServlet();
         ServletRegistration.Dynamic servlet2 = servletContext.addServlet("AvatarServlet", avatarServlet);
         servlet2.addMapping("/avatar");
@@ -96,6 +98,7 @@ public class AppInitializer implements WebApplicationInitializer {
         
     }
     
+    // Define the MultipartConfigElement for file transfer servlet
     private MultipartConfigElement getMultiPartConfig() {
         String location = "";
         long maxFileSize = -1;
