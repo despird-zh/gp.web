55818856a9117ec8647b6319461f3fc04bdf2f05
gary-dgc <garydiao@localhost>
2016-05-30 14:49:36 +0800

    add test

---
+++ src/test/java/gp/extra/bean1.java
@@ -0,0 +1,16 @@
+package gp.extra;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class bean1 {
+
+	@JsonProperty("prop1")
+	public String p1;
+	
+	@JsonProperty("prop2")
+	public String p2;
+	
+	@JsonProperty("prop3")
+	public Integer p3;
+	
+}
