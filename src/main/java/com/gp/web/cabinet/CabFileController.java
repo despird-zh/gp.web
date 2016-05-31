e63552a7b44b8ff2cf5811f3ff86761352a3f8fa
gary <gary.diao@yahoo.com>
2016-05-31 22:58:03 +0800

    fix issue

--- src/main/java/com/gp/web/cabinet/CabFileController.java
+++ src/main/java/com/gp/web/cabinet/CabFileController.java
@@ -2,19 +2,11 @@ package com.gp.web.cabinet;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.gp.audit.AccessPoint;
-import com.gp.common.Principal;
-import com.gp.core.CabinetFacade;
-import com.gp.core.GeneralResult;
-import com.gp.info.CabFolderInfo;
-import com.gp.web.ActionResult;
 import com.gp.web.BaseController;
-import com.gp.web.CustomWebUtils;
 
 /**
  * This controller wrap the operation on folder 
