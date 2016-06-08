9e1499c45d55c781196e004544dc285cf3fa9228
gary-dgc <garydiao@localhost>
2016-06-08 18:29:48 +0800

    move file and update

--- src/main/java/com/gp/core/DictionaryFacade.java
+++ src/main/java/com/gp/core/DictionaryFacade.java
@@ -1,16 +1,29 @@
 package com.gp.core;
 
+import java.util.List;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.gp.audit.AccessPoint;
+import com.gp.common.Operations;
+import com.gp.common.Principal;
+import com.gp.common.ServiceContext;
+import com.gp.common.GeneralContext.ExecState;
 import com.gp.info.DictionaryInfo;
+import com.gp.info.InfoId;
 import com.gp.svc.DictionaryService;
+import com.gp.validation.ValidationMessage;
+import com.gp.validation.ValidationUtils;
 
 @Component
 public class DictionaryFacade {
 
+	static Logger LOGGER = LoggerFactory.getLogger(MasterFacade.class);
+	
 	static DictionaryService dictservice;
 	
 	@Autowired
@@ -19,9 +32,81 @@ public class DictionaryFacade {
 		MasterFacade.dictservice = dictservice;
 	}
 	
-	public static String findDictEntry(String key, Locale locale){
+	public static String findMessagePattern(String key, Locale locale){
 		return key;
 		
 		
+	}
+	
+
+	/**
+	 * Find the dictionary entries from database.
+	 * @param groupkey the entry group key
+	 * @param language the language expected 
+	 **/
+	public static GeneralResult<List<DictionaryInfo>> findDictEntries(AccessPoint accesspoint,
+			Principal principal, String groupkey, String language){
+		
+		GeneralResult<List<DictionaryInfo>> gresult = new GeneralResult<List<DictionaryInfo>>();
+		
+		try(ServiceContext<?> svcctx = ContextHelper.beginServiceContext(principal, accesspoint,
+				Operations.FIND_DICTS)){
+
+			List<DictionaryInfo> dicts = dictservice.getDictEntries(svcctx);
+			gresult.setReturnValue(dicts);
+			gresult.setMessage("success get option groups ", true);
+			
+		} catch (Exception e) {
+			
+			LOGGER.error("Fail query option group",e);
+			ContextHelper.stampContext(e);
+			gresult.setMessage("fail get option groups ", false);
+			
+		}finally{
+			
+			ContextHelper.handleContext();
+		}
+		return gresult;
+	}
+	
+	public static GeneralResult<Boolean> saveDictEntry(AccessPoint accesspoint,
+			Principal principal, DictionaryInfo dictinfo){
+		
+		GeneralResult<Boolean> gresult = new GeneralResult<Boolean>();
+		
+		if(!InfoId.isValid(dictinfo.getInfoId())){
+			ValidationMessage noidmsg = new ValidationMessage("dict_id", "miss required dict entry id");
+			gresult.addMessage(noidmsg);
+			gresult.setMessage("fail to valid the message.", false);
+			gresult.setReturnValue(false);
+			return gresult;
+		}
+		try(ServiceContext<?> svcctx = ContextHelper.beginServiceContext(principal, accesspoint,
+				Operations.UPDATE_DICT)){
+			
+			svcctx.setAuditObject(dictinfo.getInfoId());
+			svcctx.addAuditPredicates(dictinfo);
+			
+			List<ValidationMessage> vmsg = ValidationUtils.validate(principal.getLocale(), dictinfo);
+			if(null != vmsg && vmsg.size() > 0){ // fail pass validation
+				gresult.addMessages(vmsg);				
+				svcctx.endAudit(ExecState.EXCEP, "row info validation fail");
+				return gresult;
+			}
+			
+			boolean state = dictservice.updateDictEntry(svcctx, dictinfo);
+			gresult.setReturnValue(state);
+			gresult.setMessage("success save dict entry", true);
+			
+		} catch (Exception e) {
+			
+			LOGGER.error("Fail query dict entry",e);
+			ContextHelper.stampContext(e);
+			gresult.setMessage("fail save dict entry", false);			
+		}finally{
+			
+			ContextHelper.handleContext();
+		}
+		return gresult;
 	}
 }
--- src/main/java/com/gp/exception/WebException.java
+++ src/main/java/com/gp/exception/WebException.java
@@ -9,43 +9,43 @@ import java.util.ResourceBundle;
 import org.apache.commons.lang.StringUtils;
 import com.gp.exception.BaseException;
 
-public class CoreException extends BaseException{
+public class WebException extends BaseException{
 
 	private static final long serialVersionUID = 1L;
 
-	private static Map<Locale, ResourceBundle> core_bundles = new HashMap<Locale, ResourceBundle>();
-	
-	public CoreException(String errorcode,Object ...param){
+	private static Map<Locale, ResourceBundle> web_bundles = new HashMap<Locale, ResourceBundle>();
+
+	public WebException(String errorcode,Object ...param){
 		this(Locale.getDefault(),errorcode, param);
 	}
 	
-    public CoreException(String errorcode, Throwable cause,Object ...param) {
+    public WebException(String errorcode, Throwable cause,Object ...param) {
         this(Locale.getDefault(), errorcode, cause, param);
     }
     
-	public CoreException(Locale locale, String errorcode, Object... param) {
+	public WebException(Locale locale, String errorcode, Object... param) {
 		super(errorcode, param);
-		this.message = findMessage(locale,errorcode, param);
+		this.message = findMessage(locale, errorcode, param);
 	}
 	
-    public CoreException(Locale locale, String errorcode, Throwable cause,Object ...param) {
+    public WebException(Locale locale, String errorcode, Throwable cause,Object ...param) {
         super(errorcode, cause);
         this.message = findMessage(locale,errorcode, param);
     }
     
-    public CoreException(Throwable cause) {
+    public WebException(Throwable cause) {
         super(cause);
     }
     	
     @Override
 	protected String findMessage(Locale locale, String errorcode,Object ... param){
 		
-		ResourceBundle rb = core_bundles.get(locale);
+		ResourceBundle rb = web_bundles.get(locale);
 		if(rb == null){
-			rb = loadResourceBundle(locale, CoreException.class);
-			core_bundles.put(locale, rb);
+			rb = loadResourceBundle(locale, WebException.class);
+			web_bundles.put(locale, rb);
 		}
-		String messagePattern =  (rb == null) ? errorcode : rb.getString(errorcode);
+		String messagePattern = (rb == null) ? errorcode : rb.getString(errorcode);
 		if(StringUtils.isBlank(messagePattern)){
 			return super.findMessage(locale, errorcode, param);
 		}
