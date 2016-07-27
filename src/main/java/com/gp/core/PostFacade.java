c17ea0705cb3658aaf1e9c5d0d326e5b5f31ef9f
gary-dgc <gary.diao@yahoo.com>
2016-07-27 16:58:01 +0800

    update post facade

--- src/main/java/com/gp/core/PostFacade.java
+++ src/main/java/com/gp/core/PostFacade.java
@@ -1,13 +1,19 @@
 package com.gp.core;
 
 import com.gp.audit.AccessPoint;
+import com.gp.common.Operations;
 import com.gp.common.Principal;
+import com.gp.common.ServiceContext;
 import com.gp.dao.info.PostInfo;
+import com.gp.exception.ServiceException;
+import com.gp.info.CombineInfo;
 import com.gp.info.InfoId;
 import com.gp.svc.PostService;
+import com.gp.svc.info.PostExt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,25 +22,89 @@ import java.util.List;
 @Component
 public class PostFacade {
 
-    private static PostService postService;
+    private static PostService postservice;
 
     @Autowired
-    public PostFacade(PostService postsvc){
+    public PostFacade(PostService postservice){
 
-        PostFacade.postService = postsvc;
+        PostFacade.postservice = postservice;
     }
 
-    public static List<PostInfo> findPersonalPosts(AccessPoint accesspoint,
-                                                   Principal principal,
-                                                   String state){
+    /**
+     * Find the personal post
+     **/
+    public static List<CombineInfo<PostInfo, PostExt>>  findPersonalPosts(AccessPoint accesspoint,
+                                                                          Principal principal,
+                                                                          String state, String type, String scope){
 
-        return null;
+        List<CombineInfo<PostInfo, PostExt>> result = new ArrayList<CombineInfo<PostInfo, PostExt>>();
+
+        try(ServiceContext svcctx = ContextHelper.beginServiceContext(principal, accesspoint,
+                Operations.FIND_TAGS)){
+
+            result = postservice.getPersonalPosts(svcctx, principal.getAccount(), state, type, scope);
+
+        } catch (ServiceException e)  {
+
+            ContextHelper.stampContext(e,"excp.find.personal.posts");
+
+        }finally{
+
+            ContextHelper.handleContext();
+        }
+        return result;
     }
 
+    /**
+     * find the work group posts
+     **/
     public static List<PostInfo> findWorkgroupPosts(AccessPoint accesspoint,
                                                     Principal principal,
                                                     InfoId<Long> wid,
-                                                    String state){
-        return null;
+                                                    String state, String type, String scope){
+
+        List<CombineInfo<PostInfo, PostExt>> result = new ArrayList<CombineInfo<PostInfo, PostExt>>();
+
+        try(ServiceContext svcctx = ContextHelper.beginServiceContext(principal, accesspoint,
+                Operations.FIND_TAGS)){
+
+            result = postservice.getWorkgroupPosts(svcctx, wid, state, type, scope);
+
+        } catch (ServiceException e)  {
+
+            ContextHelper.stampContext(e,"excp.find.wgroup.posts");
+
+        }finally{
+
+            ContextHelper.handleContext();
+        }
+
+        return result;
+    }
+
+    /**
+     * Find the square post
+     **/
+    public static List<PostInfo> findSquarePosts(AccessPoint accesspoint,
+                                                    Principal principal,
+                                                    String state, String type, String scope){
+
+        List<CombineInfo<PostInfo, PostExt>> result = new ArrayList<CombineInfo<PostInfo, PostExt>>();
+
+        try(ServiceContext svcctx = ContextHelper.beginServiceContext(principal, accesspoint,
+                Operations.FIND_TAGS)){
+
+            result = postservice.getSquarePosts(svcctx, state, type, scope);
+
+        } catch (ServiceException e)  {
+
+            ContextHelper.stampContext(e,"excp.find.square.posts");
+
+        }finally{
+
+            ContextHelper.handleContext();
+        }
+
+        return result;
     }
 }
