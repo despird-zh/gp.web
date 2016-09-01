a7e60127c4d59bedc8451299d7f07ddc6c95b07d
gary-dgc <gary.diao@yahoo.com>
2016-09-01 17:40:22 +0800

    add bind_process to quick_flow and flow_proc table

---
+++ src/main/java/com/gp/quickflow/PublicPostProcess.java
@@ -0,0 +1,31 @@
+package com.gp.quickflow;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import com.gp.info.InfoId;
+
+public class PublicPostProcess extends BaseFlowProcess {
+
+	Logger LOGGER = LoggerFactory.getLogger(PublicPostProcess.class);
+	
+	public PublicPostProcess(String operation) {
+		super(operation, true, true);
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	}
+
+	@Override
+	public void fail(InfoId<Long> procId, InfoId<?> resourceId, Map<String, Object> procData) {
+		LOGGER.debug("fail");
+	}
+
+	@Override
+	public void pass(InfoId<Long> procId, InfoId<?> resourceId, Map<String, Object> procData) {
+		LOGGER.debug("pass");
+	}
+
+
+}
